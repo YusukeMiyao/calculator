49e8020b7cbd17141ec5a289b27c7f0e26d0ac75
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-07-14 22:01:52 +0900

    [add]定数ファイルの作成

---
+++ src/utils/constant.jsx
@@ -0,0 +1,8 @@
+export const INPUT_NUM = "inputNum"
+export const OPERATER = "operater"
+export const POSI_AND_NEGA = "posiAndNega"
+export const ONE_HUNDREDTH = "oneHundredth"
+export const DECIMAL_POINT = "decimalPoint"
+export const CLEAR = "clear"
+export const ALL_CLEAR = "allClear"
+export const EQUAL = "equal"
